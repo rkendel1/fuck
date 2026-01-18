d85f9b8fae9713ca85d30278072fc79636525046
rkendel1 <148293244+rkendel1@users.noreply.github.com>
2026-01-18 14:52:13 -0500

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import svelte from 'rollup-plugin-svelte';
+import Tab from './src/embed-components/Tab.svelte';
 import Announcement from './src/embed-components/Announcement.svelte';
 import ThemeSelector from './src/embed-components/ThemeSelector.svelte';
 import Step from './src/embed-components/Step.svelte';
@@ -9,25 +10,25 @@ import Steps from './src/embed-components/Steps.svelte';
 
 export default {
   input: {
-    Tab: 'src/embed-components/Tab.svelte'
-  ,
-    Announcement: 'src/embed-components/Announcement.svelte'
-  ,
-    ThemeSelector: 'src/embed-components/ThemeSelector.svelte'
-  ,
-    Step: 'src/embed-components/Step.svelte'
-  ,
-    ChartToolbar: 'src/embed-components/ChartToolbar.svelte'
-  ,
-    Github: 'src/embed-components/Github.svelte'
-  ,
-    LinkedCard: 'src/embed-components/LinkedCard.svelte'
-  ,
+    Tab: 'src/embed-components/Tab.svelte',
+    Announcement: 'src/embed-components/Announcement.svelte',
+    ThemeSelector: 'src/embed-components/ThemeSelector.svelte',
+    Step: 'src/embed-components/Step.svelte',
+    ChartToolbar: 'src/embed-components/ChartToolbar.svelte',
+    Github: 'src/embed-components/Github.svelte',
+    LinkedCard: 'src/embed-components/LinkedCard.svelte',
     Steps: 'src/embed-components/Steps.svelte'
   },
   output: {
     dir: 'public',
-    format: 'esm'
+    format: 'esm',
+    sourcemap: true
   },
-  plugins: [svelte()]
+  plugins: [
+    svelte({
+      compilerOptions: {
+        customElement: true
+      }
+    })
+  ]
 };
